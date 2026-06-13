39d69ec454ee6f32f840929cc6cdd153e3462d29
Julie Weeds <juliewe@m902099.inf.susx.ac.uk>
2026-06-13 14:24:35 +0100

    Basic working app

---
+++ main.py
@@ -0,0 +1,135 @@
+import streamlit as st
+import random, time
+
+spelldata=[["naive","naieve","nave","niave","nieve"],["prove","proove","proov","proofe","porove"],["particular","particuler","partacular","particulier","particuliar"],["specific","spacefic","spacific","specefic","spacifec"],["advocate","advacate","addvocate","addvacate","advocote"],
+           ["unequivocal","inequivocal","anequivocal","unequivacal","unequivocale"],
+           ["psychological","pyschological","psychalogical","pychological","psychelogical"],
+           ["receive","recieve","receeve","reseive","recive"],
+           ["physiological","physialogical","phsyiological","pyhsiological","phisyological"],
+           ["despondent","despondant","dispondent","dispondant","depsondent"],
+           ["callous","callouse","calous","calouse","callus"]
+           ]
+spellqs=[]
+for spellingset in spelldata:
+    correct=spellingset[0]
+    random.shuffle(spellingset)
+    spellqs.append({"correct":correct,"options":spellingset})
+qs=5
+def nl(num_of_lines):
+     for i in range(num_of_lines):
+         st.write(" ")
+
+def btn_click():
+    ss.counter+=1
+    if ss.counter>=len(ss.button_label):
+        ss.counter=0
+        ss.clear()
+    else:
+        update_session_state()
+        #with st.spinner("*this may take a while*"):
+        #    time.sleep(0.1)
+
+def update_session_state():
+    if ss.counter==1:
+        ss['start'] = True
+        ss['stop']=False
+        ss.current_quiz=random.sample(spellqs,qs)
+        ss.current=0
+        ss['starttime']=time.time()
+    elif ss.counter==len(ss.button_label)-1:
+        ss['start']=False
+        ss['stop']=False
+        ss['complete']=True
+        ss['endtime']=time.time()
+    elif ss.counter%2==1:
+        #odd number of clicks
+        ss['start']=True
+        ss['stop']=False
+        ss.current+=1
+    else:
+        #even number of clicks
+        ss['start']=False
+        ss['stop']=True
+
+def quiz_app():
+#create container
+    #with st.container():
+    if ss.start or ss.stop:
+
+        current_question=ss.current+1
+        number_placeholder.write(f"**Question {current_question}: Choose the correct spelling and click submit**")
+        options=ss.current_quiz[ss.current].get("options")
+        options_placeholder.radio("",options,index=None,key=f"Q{ss.current}")
+        nl(1)
+    if ss.stop:
+
+        if ss[f'Q{ss.current}'] == ss.current_quiz[ss.current].get("correct"):
+            ss.user_answers[ss.current]=True
+            results_placeholder.success("Correct")
+        else:
+            ss.user_answers[ss.current]=False
+            results_placeholder.error("Incorrect")
+
+    if ss.complete:
+        ss['grade'] = list(ss.user_answers.values()).count(True)
+        ss['timetaken']=round(ss['endtime']-ss['starttime'])
+        scorecard_placeholder.write(f"### **Your final score : {ss['grade']} / {len(ss.current_quiz)}**\n### **Your time : {ss['timetaken']}s**")
+
+#activate session state to maintain state across refreshes
+ss=st.session_state
+#initialise variables
+if 'counter' not in ss:
+    ss['counter']=0
+if 'current' not in ss:
+    ss['current']=0
+if 'start' not in ss:
+    ss['start']=False
+if 'stop' not in ss:
+    ss['stop']=False
+if 'complete' not in ss:
+    ss['complete']=False
+if 'starttime' not in ss:
+    ss['starttime']=False
+if 'endtime' not in ss:
+    ss['endtime']=False
+if 'button_label' not in ss:
+    ss['button_label']=['Start']+['Submit','Next']*qs +['Reload']
+if 'current_quiz' not in ss:
+    ss['current_quiz']=[]
+if 'user_answers' not in ss:
+    ss['user_answers']={}
+if 'grade' not in ss:
+    ss['grade']=0
+if 'timetaken' not in ss:
+    ss['timetaken']=0
+
+
+st.title("Julie's Spelling Bee")
+nl(1)
+st.header("How good is your spelling?")
+nl(1)
+st.instructions=st.empty()
+if not ss['start']:
+    st.instructions.write(f"""
+        There will be {qs} questions.
+        For each question, select the correct spelling from the 5 options and click submit.
+        Then click next to take you to the next question or finish the quiz.
+    
+        Click start or reload to begin
+    """)
+
+#placeholder to print score
+scorecard_placeholder=st.empty()
+with st.container():
+    number_placeholder = st.empty()
+    options_placeholder = st.empty()
+    results_placeholder = st.empty()
+
+
+
+st.button(label=ss.button_label[ss.counter],key='button_press',on_click=btn_click)
+quiz_app()
+
+
+
+
